e6c41450023fec7c122b716bda27a9c746fbce25
Jok3rsGh0st <43338956+Jok3rsGh0st@users.noreply.github.com>
2019-01-23 13:07:40 +0200

    Update PokeGo.js

--- PokeGo.js
+++ PokeGo.js
@@ -133,16 +133,15 @@ bot.on("message", async message => {
   if(cmd === `${prefix}fog`){
     return message.channel.send("Ice and Steel moves are boosted");
   }
-});
 
 //Community Day Information
-if(cmd === `${prefix}CommunityDay`){
+  if(cmd === `${prefix}CommunityDay`){
     message.channel.send("Community Day: 16 Febuary 2019, 11:00 - 14:00");
     message.channel.send("___***Feature:***___ /nSwinub will appear more frequently in the wild. /nLure Modules will last 3 hours for the duration of the event.");
     message.channel.send("___***Bonuses:***___ /n3x Catch Stardust. /nEarn 5 rewards per day from Trainer Battles. /nEarn up to 5 Sinnoh Stones from Trainer Battles on Community Day.");
   }
 
-if(cmd === `${prefix}CommunityStats`){
+  if(cmd === `${prefix}CommunityStats`){
     message.channel.send("___***Mamoswine***___");
     message.channel.send("___***Weaknesses:***___ Fire 160%, Water 160%, Grass 160%, Fighting 160%, Steel 160% /n___***Resistance:***___ /nElectric 39%, Poison 63%");
     message.channel.send("___***Counters:***___ /nGroudon: Ground /Rhyperior: Ground & Rock /nWeavile: Dark & Ice");
