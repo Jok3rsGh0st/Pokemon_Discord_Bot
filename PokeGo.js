faaeea5346efe1cfbd6bc2fa86ece405bac6e967
Jok3rsGh0st <43338956+Jok3rsGh0st@users.noreply.github.com>
2019-01-23 12:48:11 +0200

    Update PokeGo.js

--- PokeGo.js
+++ PokeGo.js
@@ -135,4 +135,18 @@ bot.on("message", async message => {
   }
 });
 
+//Community Day Information
+if(cmd === `${prefix}CommunityDay`){
+    message.channel.send("Community Day: 16 Febuary 2019, 11:00 - 14:00");
+    message.channel.send("___***Feature:***___ /nSwinub will appear more frequently in the wild. /nLure Modules will last 3 hours for the duration of the event.");
+    message.channel.send("___***Bonuses:***___ /n3x Catch Stardust. /nEarn 5 rewards per day from Trainer Battles. /nEarn up to 5 Sinnoh Stones from Trainer Battles on Community Day.");
+  }
+
+if(cmd === `${prefix}CommunityStats`){
+    message.channel.send("___***Mamoswine***___");
+    message.channel.send("___***Weaknesses:***___ Fire 160%, Water 160%, Grass 160%, Fighting 160%, Steel 160% /n___***Resistance:***___ /nElectric 39%, Poison 63%");
+    message.channel.send("___***Counters:***___ /nGroudon: Ground /Rhyperior: Ground & Rock /nWeavile: Dark & Ice");
+    message.channel.send("```Type	Quick Move	DMG	EPS	DPS /nMud Slap	15	8.6	12.9 /nPowder Snow	6	15	7.2 /nType	Charge Move	DMG	EPS	DPS /nAvalanche	90	-18.5	33.8 /nBulldoze	80	-14.3	24 /nStone Edge	100	-43.5	35.7```");
+});
+
 bot.login(TokenPoGo.env.token);
